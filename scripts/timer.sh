9127879bc436d5a3ab6c53b8089ec8d7be05fbcf
Shon Verkhovskiy <Shon.Verkhovskiy@s.afeka.ac.il>
2026-06-27 21:42:08 +0300

    timer script

---
+++ scripts/timer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Please enter a time to set up a timer for in the next format: HH:MM:SS"
+read time
+
+hours=$(echo $time | cut -d: -f1)
+minutes=$(echo $time | cut -d: -f2)
+seconds=$(echo $time | cut -d: -f3)
+
+total_seconds = $( ( (hours * 3600) + (minutes * 60) + seconds) )
+
+
+while [ $total_seconds -gt 0 ]; do
+    echo -ne "\r$total_seconds left                       "
+    sleep 1
+    total_seconds=$((total_seconds - 1))
+done
+
+
+echo -ne "\r Times up!           \n"
