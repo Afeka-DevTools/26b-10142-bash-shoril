5017597b67fed408a70fd7b8b749c7ff4fd32ad3
Shon Verkhovskiy <Shon.Verkhovskiy@s.afeka.ac.il>
2026-06-27 21:52:01 +0300

    password generator script

---
+++ scripts/password_generator.sh
@@ -0,0 +1,9 @@
+#!/bi/bash
+
+charset="a-zA-Z0-9!@#$&"
+password=""
+for i in {1..10}; do
+    password+=$(tr -dc "$charset" < /dev/urandom | head -c 1)
+done
+
+echo "Your new password is: $password"
