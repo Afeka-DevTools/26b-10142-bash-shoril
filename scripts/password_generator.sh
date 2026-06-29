d3f05f96407a74c5eb4674354dc38122b9fe9f6d
Shon Verkhovskiy <Shon.Verkhovskiy@s.afeka.ac.il>
2026-06-29 15:06:16 +0300

    fixed a bug in password generator

--- scripts/password_generator.sh
+++ scripts/password_generator.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-
-charset="a-zA-Z0-9!@#$&"
+length=8
+letterset="a-zA-Z"
+numberset="0-9"
+signset="!@#$&"
 password=""
-for i in {1..10}; do
-    password+=$(tr -dc "$charset" < /dev/urandom | head -c 1)
+for i in {1..length}; do
+    password+=$(tr -dc "$letterset" < /dev/urandom | head -c 1)
 done
-
+password+=$(tr -dc "$numberset" < /dev/urandom | head -c 1)
+password+=$(tr -dc "$signset" < /dev/urandom | head -c 1)
+shuffled_password=$(echo "$password" | fold -w1 | shuf | tr -d '\n')
 echo "Your new password is: $password"
