82b0b074f427570a8a1b99647e7d216cd9794b4c
Shon Verkhovskiy <Shon.Verkhovskiy@s.afeka.ac.il>
2026-06-29 15:26:44 +0300

    final password generator script

--- scripts/password_generator.sh
+++ scripts/password_generator.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-length=8
+
 letterset="a-zA-Z"
 numberset="0-9"
 signset="!@#$&"
 password=""
-for i in {1..length}; do
+for i in {1..8}; do
     password+=$(tr -dc "$letterset" < /dev/urandom | head -c 1)
 done
 password+=$(tr -dc "$numberset" < /dev/urandom | head -c 1)
 password+=$(tr -dc "$signset" < /dev/urandom | head -c 1)
 shuffled_password=$(echo "$password" | fold -w1 | shuf | tr -d '\n')
-echo "Your new password is: $password"
+
+
+echo "Your new password is: $shuffled_password"    
